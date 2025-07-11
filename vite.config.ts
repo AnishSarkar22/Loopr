08be049d489340652c9cf50d41ee6549290cda65
Anish Sarkar <104695310+AnishSarkar22@users.noreply.github.com>
2025-07-12 02:43:23 +0530

    feat: update favicon to SVG format and add PWA assets

--- vite.config.ts
+++ vite.config.ts
@@ -10,7 +10,7 @@ export default defineConfig({
         VitePWA({
             registerType: 'autoUpdate',
             includeAssets: [
-                'favicon.png',
+                'favicon.svg',
                 'pwa-192x192.png',
                 'pwa-512x512.png',
                 'robots.txt'
